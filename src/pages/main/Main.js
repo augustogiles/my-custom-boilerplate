520553e5638155a6b00584f45e332bb38835d1c8
Augusto Costa <augustogiles@gmail.com>
2020-04-12 02:39:11 +0100

    adding linter and linting

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -2,13 +2,13 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Title = styled.h1`
-    color: black;
-    font-size: 2.5rem;
-    font-weight: 700;
+  color: black;
+  font-size: 2.5rem;
+  font-weight: 700;
 `;
 
 const Main = () => {
-  return (<Title>Hello from React</Title>);
-}
+  return <Title>Hello from React</Title>;
+};
 
 export default Main;
