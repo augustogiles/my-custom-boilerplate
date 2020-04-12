f5dd37b688f058e2e947a7d40b9c7a352d542cac
Augusto Costa <augustogiles@gmail.com>
2020-04-12 02:04:04 +0100

    adding styled-components lib

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+    color: black;
+    font-size: 2.5rem;
+    font-weight: 700;
+`;
 
 const Main = () => {
-  return (<h1>Hello from React</h1>);
+  return (<Title>Hello from React</Title>);
 }
 
 export default Main;
